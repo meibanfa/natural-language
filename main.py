81cc28413b8b84cab333450c36a3f14bda0e1242
grool9 <zhangruiqi2@qq.com>
2017-05-01 21:37:06 +0800

    complete getVector

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
+import word2vec
 import numpy as np
 
-# load the data
+## description ##
+def getVector(filename):
+    # train
+    word2vec.word2phrase(filename, 'text-phrase', verbose=True)
+    word2vec.word2vec('text-phrase', 'text.bin', size = 100, verbose = True)
 
+    # prediction
+    model = word2vec.load('text.bin', encoding="ISO-8859-1")
 
+    (m, n) = model.vectors.shape
+    temp = np.ones([1, m])
+    result = np.dot(temp, model.vectors)
+    return result
 
-# word2vec
-
-
+## input
+l1 = getVector('nl1.txt')
