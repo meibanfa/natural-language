eff6eddbeca2b80e63d804f9721f54bdcffc8d09
grool9 <zhangruiqi2@qq.com>
2017-05-06 11:31:16 +0800

    .

--- main.py
+++ main.py
@@ -1,19 +1,68 @@
-import word2vec
+import os
+import keras
 import numpy as np
+from keras.preprocessing.text import text_to_word_sequence
+from keras.preprocessing.sequence import pad_sequences
 
-## description ##
-def getVector(filename):
-    # train
-    word2vec.word2phrase(filename, 'text-phrase', verbose=True)
-    word2vec.word2vec('text-phrase', 'text.bin', size = 100, verbose = True)
+TEXT_DATA_DIR = 'text_data'
+texts = []
+l = [] #input
+'''
+### 处理文本 ####
+for name in sorted(os.listdir(TEXT_DATA_DIR)):
+    if name[0].isdigit():
+        path = os.path.join(TEXT_DATA_DIR, name)
+        f = open(path)
+        texts.append(f.read())
+        f.close()
 
-    # prediction
-    model = word2vec.load('text.bin', encoding="ISO-8859-1")
+# word2vec dictionary
+embeddings_index = {}
+f = open(os.path.join('glove.6B', 'glove.6B.100d.txt'))
+for line in f:
+    values = line.split()
+    word = values[0]
+    coefs = np.asarray(values[1:], dtype = 'float32')
+    embeddings_index[word] = coefs
+f.close()
 
-    (m, n) = model.vectors.shape
-    temp = np.ones([1, m])
-    result = np.dot(temp, model.vectors)
-    return result
+for text in texts:
+    words = keras.preprocessing.text.text_to_word_sequence(text,
+        filters= '!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n', lower=True, split=" ")
+    num = np.shape(words)[0]
+    vector = np.zeros(100)
+    for w in words:
+        if w in embeddings_index.keys():
+            v = embeddings_index[w]
+            vector += v
+    vector /= num
+    vector = np.ndarray.tolist(vector)
+    l.append(vector)
+'''
+### 处理代码 ###
+J = 5
+CODE_DATA_DIR = 'code_data'
+codes = []
 
-## input
-l1 = getVector('nl1.txt')
+codes_vector = []
+H = []
+
+c = [] #input
+
+for name in sorted(os.listdir(CODE_DATA_DIR)):
+    if name[0].isdigit():
+        path = os.path.join(CODE_DATA_DIR, name)
+        f = open(path)
+        codes.append(f.read())
+        f.close()
+
+for code in codes:
+    tokens = keras.preprocessing.text.text_to_word_sequence(code,
+        filters= '\t\n', lower=True, split=" ")
+    v = np.random.normal(size=100) + 1
+    codes_vector.append(v)
+    h = np.zeros([100, 100])
+    for i in range(100):
+        h[i][i] = 1/J
+    H.append(h)
+
